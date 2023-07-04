f5596e4c31d2f83dc590256c5664b4925628ebca
jaimonjacob <jaimon.jacob2009@gmail.com>
2023-07-04 19:38:41 +0000

    new

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 from testinput import greet_user
+
+
+def welcome_user():
+    '''Take inputs from the user and welcome him/her'''
+    print('Plesae enter your first and last name: \n')
+    while True:
+        first_name = input('please enter first name: \n')
+        if first_name == 'q':
+            break
+        second_name = input('please enter second name: \n')
+        if first_name == 'q':
+            break
+        formatted_name = greet_user(first_name, second_name)
+        print(f'Hellow {formatted_name}')
+
+welcome_user()
+
