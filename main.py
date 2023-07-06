677544a39b7af0eb6184bb53188794a6ea09fca8
jaimonjacob <jaimon.jacob2009@gmail.com>
2023-07-06 10:11:41 +0000

    Updates

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from testinput import greet_user
+from testinput import greet_user, Anonymoussurvey
 
-def welcome_user():
-    '''Take inputs from the user and welcome him/her'''
-    print('Plesae enter your first and last name: \n')
-    while True:
-        first_name = input('please enter first name: \n')
-        if first_name == 'q':
-            break
-        second_name = input('please enter second name: \n')
-        if first_name == 'q':
-            break
-        formatted_name = greet_user(first_name, second_name)
-        print(f'Hellow {formatted_name}')
+# def welcome_user():
+#     '''Take inputs from the user and welcome him/her'''
+#     print('Plesae enter your first and last name: \n')
+#     while True:
+#         first_name = input('please enter first name: \n')
+#         if first_name == 'q':
+#             break
+#         second_name = input('please enter second name: \n')
+#         if first_name == 'q':
+#             break
+#         formatted_name = greet_user(first_name, second_name)
+#         print(f'Hellow {formatted_name}')
 
-welcome_user()
+# welcome_user()
+
+survey_one = Anonymoussurvey('what\'s my name?')
+survey_one.show_question()
+survey_one.add_responses('john doe')
+print(vars(survey_one))
+
 
--- testinput.py
+++ testinput.py
@@ -8,3 +8,26 @@ def greet_user(first, last, middle = ''):
 
 print(greet_user('John', 'Doe', 'Henry'))
 
+class Anonymoussurvey:
+  '''collect anonymous survey reuslts'''
+
+  def __init__(self, question):
+    '''Store question and store resposnes'''
+    self.question = question
+    self.responses = []
+  
+  def show_question(self):
+    '''show the question'''
+    print(self.question)
+
+  def add_responses(self, new_response):
+    '''Adds resposnes'''
+    self.responses.append(new_response)
+
+  def show_results(self):
+    '''show the results'''
+    print('The responses are')
+    for item in self.responses:
+      print(f'{item}')  
+
+
